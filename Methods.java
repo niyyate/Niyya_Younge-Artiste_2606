c237e2c522ae6ba25d35daea949a3a3c5732b532
Niyya Artiste <n.artiste13@gmail.com>
2016-12-08 17:43:47 -0500

    half way done

--- Methods.java
+++ Methods.java
@@ -1,5 +1,37 @@
-Each Functional Requirement should have 
+//Each Functional Requirement should have 
 1. Method Name 
 2. Input parameters 
 3. Return Type
 ex. int place_order(int product_id, int quantity);
+
+void create_user(String userName, String password, String firstName, String lastName, String email, int phoneNumber);
+   void add_Addr(String userName, String address_1, String address_2, String city, String state, int zip);
+   void add_Billing(String userName, String b_Address_1, String b_Address_2, String b_City, String b_State, int b_Zip, int ccnum, int sec_code);
+
+String validate(String userName, String userType){
+return userType;}
+
+void delete(String userName, String employeeNum);
+
+String[] search_books(String title){
+return bookTitle;}
+   String[] search_books(int isbn){
+   return bookTitle};
+
+int buy_book(String username, int bookNum, String password){
+return transNum};
+
+String[] search_Trans(String userName, String password){
+return trans};
+
+int input_book(String bookName, String author, int isbnNum, int usageType, double price){
+return bookNum};
+  int input_book(String bookName, String author, int isbnNum, int usageType, double price, int rentTime){
+  return bookNum};
+
+int change_price(String bookNum, double newPrice){
+return bookNum);
+  
+  
+
+
