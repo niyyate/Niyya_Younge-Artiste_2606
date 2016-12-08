94b26c5f217890cef7f4114fb4702c554c08fe0c
Niyya Artiste <n.artiste13@gmail.com>
2016-12-08 17:57:51 -0500

    1 Method Left !!
    Refund

--- Methods.java
+++ Methods.java
@@ -5,8 +5,7 @@
 ex. int place_order(int product_id, int quantity);
 
 void create_user(String userName, String password, String firstName, String lastName, String email, int phoneNumber);
-   void add_Addr(String userName, String address_1, String address_2, String city, String state, int zip);
-   void add_Billing(String userName, String b_Address_1, String b_Address_2, String b_City, String b_State, int b_Zip, int ccnum, int sec_code);
+void add_Addr(String userName, String address_1, String address_2, String city, String state, int zip);void add_Billing(String userName, String b_Address_1, String b_Address_2, String b_City, String b_State, int b_Zip, int ccnum, int sec_code);
 
 String validate(String userName, String userType){
 return userType;}
@@ -16,22 +15,33 @@ void delete(String userName, String employeeNum);
 String[] search_books(String title){
 return bookTitle;}
    String[] search_books(int isbn){
-   return bookTitle};
+   return bookTitle;}
 
 int buy_book(String username, int bookNum, String password){
-return transNum};
+return transNum;}
 
 String[] search_Trans(String userName, String password){
-return trans};
+return transNum;}
 
 int input_book(String bookName, String author, int isbnNum, int usageType, double price){
-return bookNum};
+return bookNum;}
   int input_book(String bookName, String author, int isbnNum, int usageType, double price, int rentTime){
-  return bookNum};
+  return bookNum;}
 
 int change_price(String bookNum, double newPrice){
-return bookNum);
-  
+return bookNum;}
+
+String[] search_kiosk(int kioskNum){
+return kioskinfo;}
+   String[] search_kiosk(int kioskNum, String school){
+   return kiosksInTake;}
+   
+String[] account_hist(String userName, String password){
+return new ValueHolder(transNum, bookNum)};   
+
+
+
+
   
 
 
